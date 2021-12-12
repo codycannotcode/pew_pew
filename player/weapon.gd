de0159cdab82e1fcac7e43e46c1e0bf7796382ec
Sanmitr564 <generic39542@gmail.com>
2021-12-11 23:14:02 -0800

    wand points toward mouse

--- player/weapon.gd
+++ player/weapon.gd
@@ -13,6 +13,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var _mousePosition = get_local_mouse_position()
-	rotation = (atan2(_mousePosition.y,_mousePosition.x))
-	print(atan2(_mousePosition.y,_mousePosition.x))
+	var _mousePosition = get_global_mouse_position()
+	look_at(_mousePosition)
+	
