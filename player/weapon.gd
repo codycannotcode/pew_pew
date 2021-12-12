6762e230f886f83d87200aace41c05424deb5387
Sanmitr564 <generic39542@gmail.com>
2021-12-11 22:50:30 -0800

    failed rotation code

---
+++ player/weapon.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var _mousePosition = get_local_mouse_position()
+	rotation = (atan2(_mousePosition.y,_mousePosition.x))
+	print(atan2(_mousePosition.y,_mousePosition.x))
